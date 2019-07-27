2b32282eff97da187310a24ee5dd4886a0e32fd1
Michael Ernst <mernst@cs.washington.edu>
2019-07-26 21:37:07 -0700

    More debugging

--- checker/bin-devel/test-all-tests.sh
+++ checker/bin-devel/test-all-tests.sh
@@ -10,6 +10,7 @@ echo "CHECKERFRAMEWORK=$CHECKERFRAMEWORK"
 
 # For debugging
 git branch
+git branch -a
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPTDIR/build.sh ${BUILDJDK}
