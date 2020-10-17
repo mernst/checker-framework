906e8c537fc40aedca277a84bb4fc6c8a9815057
Michael Ernst <mernst@cs.washington.edu>
2020-10-17 11:19:56 -0700

    Run JSpecify tests in CI

--- checker/bin-devel/test-cftests-junit.sh
+++ checker/bin-devel/test-cftests-junit.sh
@@ -14,3 +14,17 @@ source "$SCRIPTDIR"/build.sh
 
 
 ./gradlew test --console=plain --warning-mode=all --no-daemon
+
+# TODO: This is a hack, there should be a gradle command-line argument such as -PrunNullnessJspecifySamplesTest
+if type -p java; then
+  _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+  _java="$JAVA_HOME/bin/java"
+fi
+if [[ "$_java" ]]; then
+  version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+  # shellcheck disable=SC2071
+  if [[ "$version" > "9" ]]; then
+    ./gradlew NullnessJspecifySamplesTest
+  fi
+fi
