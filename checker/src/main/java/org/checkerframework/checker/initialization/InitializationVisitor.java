fed4a1f6a36d4e94442d0b79ddd78aa8538a95f3
Michael Ernst <mernst@cs.washington.edu>
2021-02-27 18:25:12 -0800

    Undo change

--- checker/src/main/java/org/checkerframework/checker/initialization/InitializationVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/initialization/InitializationVisitor.java
@@ -478,8 +478,8 @@ public class InitializationVisitor<
         super.checkAccessAllowed(field, receiverType, accessTree);
 
         if (receiverType == null) {
-            throw new BugInCF("Why is receiverType null?");
-            // return;
+            // Field is static
+            return;
         }
 
         Tree statement = this.enclosingStatement(accessTree);
