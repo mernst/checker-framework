a450e6f7796e57b3b98e10bd4cb72c33b5531d1e
Michael Ernst <mernst@cs.washington.edu>
2021-04-22 09:28:50 -0700

    Javadoc

--- checker/src/main/java/org/checkerframework/checker/enhancedfor/EnhancedForChecker.java
+++ checker/src/main/java/org/checkerframework/checker/enhancedfor/EnhancedForChecker.java
@@ -4,6 +4,11 @@ import java.util.Iterator;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.qual.RelevantJavaTypes;
 
+/**
+ * A checker that can forbid use of the enhanced {@code for} statement (foreach loops).
+ *
+ * @checker_framework.manual #enhancedfor-checker Enhanced For Checker
+ */
 @RelevantJavaTypes({
   Iterator.class,
   Iterable.class,
