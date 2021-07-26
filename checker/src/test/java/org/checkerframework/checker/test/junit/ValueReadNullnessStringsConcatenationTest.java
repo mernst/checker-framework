752ba989997988537a85600d2d1689fa6482f730
Michael Ernst <mernst@cs.washington.edu>
2021-07-26 07:07:45 -0700

    Tests for running the Value Checker and the Nullness Checker together

---
+++ checker/src/test/java/org/checkerframework/checker/test/junit/ValueReadNullnessStringsConcatenationTest.java
@@ -0,0 +1,35 @@
+package org.checkerframework.framework.test.junit;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import org.checkerframework.common.value.ValueChecker;
+import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
+import org.junit.runners.Parameterized.Parameters;
+
+public class ValueReadNullnessStringsConcatenationTest extends CheckerFrameworkPerDirectoryTest {
+
+  /** @param testFiles the files containing test code, which will be type-checked */
+  public ValueReadNullnessStringsConcatenationTest(List<File> testFiles) {
+    super(
+        testFiles,
+        List.of(
+            org.checkerframework.common.value.ValueChecker.class.getCanonicalName(),
+            org.checkerframework.checker.nullness.NullnessChecker.class.getCanonicalName()),
+        "value-and-nullness-strings-concatenation",
+        Collections.emptyList(),
+        "-Anomsgtext",
+        "-A" + ValueChecker.REPORT_EVAL_WARNS);
+    new Error().printStackTrace(System.out);
+  }
+
+  @Parameters
+  public static String[] getTestDirs() {
+    return new String[] {
+      // Not "all-systems" because This runs two type-checkers and the second one won't run if the
+      // first one issues any (expected) warnings.
+      // "all-systems",
+      "value-and-nullness-strings-concatenation"
+    };
+  }
+}
