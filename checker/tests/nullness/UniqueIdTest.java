192382e017eaf771168a1619e8d2117e4371d267
Michael Ernst <mernst@cs.washington.edu>
2022-09-07 21:36:20 -0700

    Fix compilation errors

--- checker/tests/nullness/UniqueIdTest.java
+++ checker/tests/nullness/UniqueIdTest.java
@@ -2,7 +2,8 @@ import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 
 interface UniqueIdTest {
 
-  public default String getClassAndUid(@UnknownInitialization(UniqueId.class) UniqueId this) {
-    return this.getClass().getSimpleName() + "#" + getUid();
+  public default String getClassAndUid(
+      @UnknownInitialization(UniqueIdTest.class) UniqueIdTest this) {
+    return this.getClass().getSimpleName() + "#";
   }
 }
