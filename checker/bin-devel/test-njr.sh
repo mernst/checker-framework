548318f92969aa5de8e9311ea47352bf42ab6cc7
Michael Ernst <mernst@cs.washington.edu>
2024-12-10 20:55:03 -0800

    Split into parts and run in CI

--- checker/bin-devel/test-njr.sh
+++ checker/bin-devel/test-njr.sh
@@ -2,12 +2,15 @@
 # bash because of readarray
 
 # Run WPI (for resource leaks) over the NJR test suite.
+# The optional argument can be "part1", "part2", "part3", or "part4".
 
 set -e
 set -o verbose
 set -o xtrace
 export SHELLOPTS
 echo "SHELLOPTS=${SHELLOPTS}"
+
+part=$1
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export ORG_GRADLE_PROJECT_useJdk17Compiler=true
@@ -20,7 +23,16 @@ source "$SCRIPTDIR"/test-njr-install.sh
 WPI_STDOUT="$NJR"/wpi-stdout
 mkdir -p "$WPI_STDOUT"
 cd "$NJR"
-readarray -d '' dirs < <(printf '%s\0' "$NJR"/final_dataset/* | sort -zV)
+readarray -d '' dirs < <(printf '%s\0' "$NJR"/final_dataset/* | sort -z)
+
+case $part in
+  "part1") readarray -d '' dirs < <(printf '%s\0' "$NJR"/final_dataset/url[0-3]* | sort -z) ;;
+  "part2") readarray -d '' dirs < <(printf '%s\0' "$NJR"/final_dataset/url[4-7]* | sort -z) ;;
+  "part3") readarray -d '' dirs < <(printf '%s\0' "$NJR"/final_dataset/url[8-b]* | sort -z) ;;
+  "part4") readarray -d '' dirs < <(printf '%s\0' "$NJR"/final_dataset/url[c-f]* | sort -z) ;;
+  *)       readarray -d '' dirs < <(printf '%s\0' "$NJR"/final_dataset/*         | sort -z) ;;
+esac
+
 for dir in "${dirs[@]}"; do
   dir_basename="$(basename "$dir")"
   stdout_file="${WPI_STDOUT}/${dir_basename}-wpi-stdout.txt"
