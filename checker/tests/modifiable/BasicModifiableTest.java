ed37f721f31e5da9ecf863d1c2562fd1926090ed
Michael Ernst <mernst@cs.washington.edu>
2025-10-21 19:46:54 -0700

    Remove comments

--- checker/tests/modifiable/BasicModifiableTest.java
+++ checker/tests/modifiable/BasicModifiableTest.java
@@ -7,29 +7,29 @@ public class BasicModifiableTest {
   void testBasicModifiable() {
     // Modifiable collections should allow mutation (default)
     List<String> modifiableList = new ArrayList<>();
-    modifiableList.add("test"); // OK
+    modifiableList.add("test");
 
     // Unmodifiable collections should not allow mutation
     @Unmodifiable List<String> unmodifiableList = List.of("test1", "test2");
     // :: error: (method.invocation)
-    unmodifiableList.add("test3"); // Should error
+    unmodifiableList.add("test3");
   }
 
   void testUnmodifiableFactoryMethods() {
     // These should be inferred as @Unmodifiable
     List<String> list1 = List.of("a", "b");
     // :: error: (method.invocation)
-    list1.add("c"); // Should error
+    list1.add("c");
 
     List<String> list2 = List.copyOf(new ArrayList<>());
     // :: error: (method.invocation)
-    list2.remove(0); // Should error
+    list2.remove(0);
   }
 
   void testModifiableFactoryMethods() {
     // This should be inferred as @Modifiable
     List<String> modifiableList = new ArrayList<>();
-    modifiableList.add("test"); // Should be OK
-    modifiableList.remove(0); // Should be OK
+    modifiableList.add("test");
+    modifiableList.remove(0);
   }
 }
